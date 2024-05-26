1155807a2163f67ff757b7d4293606e75bd8d947
Rodica <erodia.popa@gmail.com>
2024-05-27 01:05:22 +0300

    Implemented the FakeDataManager and create the first scenario

--- src/main/java/online/tekwillacademy/TestRunner.java
+++ src/main/java/online/tekwillacademy/TestRunner.java
@@ -1,8 +1,11 @@
 package online.tekwillacademy;
 
+import online.tekwillacademy.managers.DataGeneratorManager;
 import online.tekwillacademy.managers.DriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import online.tekwillacademy.managers.ScrollManager;
+import org.openqa.selenium.*;
+
+import javax.swing.plaf.TableHeaderUI;
 
 public class TestRunner {
     public static void main(String[] args) throws InterruptedException {
@@ -13,16 +16,51 @@ public class TestRunner {
         //  WebDriver driver = DriverManager.getInstance().getDriver();
 
         String currentTabName = driver.getWindowHandle();
-        Thread.sleep(5000);
 
         driver.switchTo().newWindow(WindowType.TAB);
-        driver.get("https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-safari-driver/4.20.0");
+
+        driver.get("https://tekwillacademy-opencart.online/");
+        System.out.println("The current URL is " + driver.getCurrentUrl());
+        System.out.println("The current page title " + driver.getTitle());
+
+        WebElement userDropDownIcon = driver.findElement(By.xpath("//i[@class='fa-solid fa-user']"));
+        userDropDownIcon.click();
+
+        WebElement registerOption = driver.findElement(By.xpath("//a[@class='dropdown-item'][normalize-space()='Register']"));
+        registerOption.click();
+
+        System.out.println("The current URL is " + driver.getCurrentUrl());
+        System.out.println("The current page title " + driver.getTitle());
+
+        WebElement firstNameInput = driver.findElement(By.id("input-firstname"));
+        firstNameInput.sendKeys("Gabriel");
+
+        WebElement lastNameInput = driver.findElement(By.id("input-lastname"));
+        lastNameInput.sendKeys("Popa");
+
+        WebElement emailInput = driver.findElement(By.id("input-email"));
+        String emailData = DataGeneratorManager.getRandomEmail();
+        emailInput.sendKeys(emailData);
+        System.out.println("Email:" + emailData);
+
+
+        WebElement passwordInput = driver.findElement(By.id("input-password"));
+        passwordInput.sendKeys("Qwerty111");
+
+        WebElement privacyToggle = driver.findElement(By.xpath("//input[@name='agree']"));
+        ScrollManager.scrollToElement(privacyToggle);
+        privacyToggle.click();
+
+        WebElement continueButton = driver.findElement(By.cssSelector("button[type='submit']"));
+        continueButton.click();
+
         Thread.sleep(5000);
+        System.out.println("The current URL is " + driver.getCurrentUrl());
+        System.out.println("The current page title " + driver.getTitle());
         driver.close();
 
         driver.switchTo().window(currentTabName);
         driver.get("https://tekwill.md/tekwill-academy/");
-        Thread.sleep(1000);
         driver.quit();
 
         System.out.println("The test is finished and the driver is closed.");
